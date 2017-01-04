de600f5726f6cdcfdbc6b173ef1aa78304474a54
AlexeyDsov <alexeydsov@gmail.com>
2017-01-04 20:25:12 +0300

    implemented priority parameter for tag bo_shurik_telegram_bot.command

--- DependencyInjection/Compiler/CommandCompilerPass.php
+++ DependencyInjection/Compiler/CommandCompilerPass.php
@@ -20,12 +20,48 @@ class CommandCompilerPass implements CompilerPassInterface
     {
         $pool = $container->getDefinition('bo_shurik_telegram_bot.command_pool');
 
+        array_map(
+            function ($serviceAndTag) use ($pool) {
+                list($id, $tag) = $serviceAndTag;
+
+                $pool->addMethodCall('addCommand', [new Reference($id)]);
+            },
+            $this->getOrderedServices($container)
+        );
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @return string[]
+     */
+    private function getOrderedServices(ContainerBuilder $container)
+    {
+        $servicesAndTags = $this->getServicesAndTags($container);
+
+        usort(
+            $servicesAndTags,
+            function ($left, $right) {
+                return ($right[1]['priority'] ?? 0) <=> ($left[1]['priority'] ?? 0);
+            }
+        );
+
+        return $servicesAndTags;
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @return string[]
+     */
+    private function getServicesAndTags(ContainerBuilder $container)
+    {
+        $services = [];
+
         foreach ($container->findTaggedServiceIds('bo_shurik_telegram_bot.command') as $id => $tags) {
             foreach ($tags as $tag) {
-                $pool->addMethodCall('addCommand', array(
-                    new Reference($id),
-                ));
+                $services[] = [$id, $tag];
             }
         }
+
+        return $services;
     }
 }
